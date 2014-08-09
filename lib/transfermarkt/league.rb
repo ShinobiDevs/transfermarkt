e39ccd50367abba7775030ebdefe1eb517e53d41
caduri katzav <caduri@shinobidevs.com>
2014-08-09 16:40:47 +0300

    discards cup competitions

--- lib/transfermarkt/league.rb
+++ lib/transfermarkt/league.rb
@@ -13,24 +13,28 @@ module Transfermarkt
         nil
       else
         league_html = Nokogiri::HTML(req.parsed_response)
-        options = {}
-
-        options[:league_uri] = league_uri
-        league_name = league_html.xpath('//select[@id="wettbewerb_select_breadcrumb"]//option[@selected="selected"]')
-        if league_name.empty?
-          options[:name] = league_html.xpath('//div[@class="spielername-profil"]').text.strip
-          options[:country] = league_html.xpath('//table[@class="profilheader"]//img/@title').first.value
+        if league_html.xpath('//table[@class="profilheader"]//tr[1]//th[1]')[0].text == "Type of cup:"
+          nil
         else
-          options[:name] = league_name[0].text
-          options[:country] = league_html.xpath('//select[@id="land_select_breadcrumb"]//option[@selected="selected"]').text
+          options = {}
+
+          options[:league_uri] = league_uri
+          league_name = league_html.xpath('//select[@id="wettbewerb_select_breadcrumb"]//option[@selected="selected"]')
+          if league_name.empty?
+            options[:name] = league_html.xpath('//div[@class="spielername-profil"]').text.strip
+            options[:country] = league_html.xpath('//table[@class="profilheader"]//img/@title').first.value
+          else
+            options[:name] = league_name[0].text
+            options[:country] = league_html.xpath('//select[@id="land_select_breadcrumb"]//option[@selected="selected"]').text
+          end
+          club_uris = league_html.xpath('//*[@id="yw1"]//table//tr//td[2]//a[1]').collect{|player_html| player_html["href"]}
+          club_names = league_html.xpath('//*[@id="yw1"]//table//tr//td[2]//a[1]').collect{|player_html| player_html.text }
+
+          clubs = Hash[club_names.zip(club_uris)]
+
+          options[:clubs_index] = clubs
+          self.new(options)
         end
-        club_uris = league_html.xpath('//*[@id="yw1"]//table//tr//td[2]//a[1]').collect{|player_html| player_html["href"]}
-        club_names = league_html.xpath('//*[@id="yw1"]//table//tr//td[2]//a[1]').collect{|player_html| player_html.text }
-
-        clubs = Hash[club_names.zip(club_uris)]
-
-        options[:clubs_index] = clubs
-        self.new(options)
       end
     end
 
